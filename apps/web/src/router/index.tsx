1005135edb466117d9c3ba64ad04703b2825b209
niyonggui <niyg@mw-robot.com>
2023-10-17 17:36:55 +0800

    feat: 完成基座改造

--- apps/web/src/router/index.tsx
+++ apps/web/src/router/index.tsx
@@ -1,41 +1,63 @@
-import { ComponentType,type FC, lazy, Suspense } from 'react'
-import { Navigate, useRoutes } from 'react-router-dom'
-import { type ILayoutData, Layout, NotFound, PageLoading,type TLayoutRoutes } from 'ui'
+import { ComponentType, type FC, lazy, Suspense } from "react";
+import { Navigate, useRoutes } from "react-router-dom";
+import {
+  type ILayoutData,
+  Layout,
+  NotFound,
+  PageLoading,
+  type TLayoutRoutes,
+} from "ui";
 
+import MicroApp from "@/components/microApp";
 /**
  * @description: 自动生成后台Layout下的routes 要求页面定义在src/pages目录下, 每个页面都有个page.ts定义路由元信息.如果存在嵌套路由, 需要按照src/pages/** /c-pages/** /index.tsx的方式定义
  * @return {*}
  */
 const generatePageRoutes = () => {
-  const pageModule = import.meta.glob('../pages/**/page.{ts,tsx}', {
+  const pageModule = import.meta.glob("../pages/**/page.{ts,tsx}", {
     eager: true,
-    import: 'default'
-  })
-  const asyncComponentModule = import.meta.glob<boolean, string, { default: ComponentType<any> }>(
-    '../pages/**/index.tsx'
-  )
-  const notPageNameStrings = ['..', '.', 'pages', 'page.ts', 'page.tsx', 'c-pages']
+    import: "default",
+  });
+  const asyncComponentModule = import.meta.glob<
+    boolean,
+    string,
+    { default: ComponentType<any> }
+  >("../pages/**/index.tsx");
+  const notPageNameStrings = [
+    "..",
+    ".",
+    "pages",
+    "page.ts",
+    "page.tsx",
+    "c-pages",
+  ];
 
-  const pageModuleEntries = Object.entries(pageModule)
-  const routes: TLayoutRoutes = []
+  const pageModuleEntries = Object.entries(pageModule);
+  const routes: TLayoutRoutes = [];
   const pagesInfo = pageModuleEntries
     .map(([pageModulePath, meta]) => {
-      const pageNameStrings = pageModulePath.split('/').filter((x) => !notPageNameStrings.includes(x))
+      const pageNameStrings = pageModulePath
+        .split("/")
+        .filter((x) => !notPageNameStrings.includes(x));
       return {
         path: pageNameStrings,
         pageModulePath,
-        meta: meta as ILayoutData
-      }
+        meta: meta as ILayoutData,
+      };
     })
     .sort((a, b) => {
       if (a.path.length === b.path.length && a.meta.order && b.meta.order) {
-        return a.meta.order - b.meta.order
+        return a.meta.order - b.meta.order;
       }
-      return a.path.length - b.path.length
-    })
+      return a.path.length - b.path.length;
+    });
   pagesInfo.forEach((pageInfo) => {
-    const parentRoute = routes.find((route) => route.path === pageInfo.path[0])
-    const Component = lazy(asyncComponentModule[pageInfo.pageModulePath.replace(/page.ts[x]?/, 'index.tsx')])
+    const parentRoute = routes.find((route) => route.path === pageInfo.path[0]);
+    const Component = lazy(
+      asyncComponentModule[
+        pageInfo.pageModulePath.replace(/page.ts[x]?/, "index.tsx")
+      ]
+    );
     const route = {
       path: pageInfo.path.at(-1),
       element: (
@@ -44,43 +66,59 @@ const generatePageRoutes = () => {
         </Suspense>
       ),
       ...pageInfo.meta,
-      children: []
-    }
+      children: [],
+    };
     if (parentRoute) {
-      parentRoute.children!.push(route)
+      parentRoute.children!.push(route);
     } else {
-      routes.push(route)
+      routes.push(route);
     }
-  })
+  });
 
-  return routes
-}
-const defineRoutes = [...generatePageRoutes()]
+  return routes;
+};
+const defineRoutes = [...generatePageRoutes()];
 
 const routes: TLayoutRoutes = [
   {
-    path: '/',
-    element: <Navigate to={'/home'} />,
-    name: '重定向',
-    id: 'redirect'
+    path: "/",
+    element: <Navigate to={"/home"} />,
+    name: "重定向",
+    id: "redirect",
   },
   {
     element: <Layout routes={defineRoutes} />,
-    id: 'layout',
-    name: '布局',
-    children: defineRoutes
+    id: "layout",
+    name: "布局",
+    children: [
+      ...defineRoutes,
+      {
+        menu: true,
+        title: "demo-react-1",
+        path: "demo-react-1",
+        element: (
+          <MicroApp
+            name="demo-react-1"
+            sandbox={{
+              experimentalStyleIsolation: true,
+            }}
+            props={{}}
+          />
+        ),
+      },
+    ],
   },
 
   {
-    path: '*',
+    path: "*",
     element: <NotFound />,
-    name: '404',
-    id: 'notfound'
-  }
-]
+    name: "404",
+    id: "notfound",
+  },
+];
 
 const RouterConfig: FC = () => {
-  return useRoutes(routes)
-}
+  return useRoutes(routes);
+};
 
-export default RouterConfig
+export default RouterConfig;
