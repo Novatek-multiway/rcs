ca7ac27cb82f9176a48f3245a560e906698bf4c5
niyonggui <niyg@mw-robot.com>
2023-10-17 17:36:55 +0800

    feat: 完成基座改造

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,15 +1,22 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { getConfig } from "@packages/build/vite.config";
+import { defineConfig, mergeConfig } from "vite";
+const sharedConfig = getConfig({
+  type: "react",
+  dirname: __dirname,
+});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://120.79.8.215:5200',
-        secure: false
-      }
-    }
-  }
-})
+export default defineConfig(
+  mergeConfig(sharedConfig, {
+    server: {
+      port: 5200,
+      proxy: {
+        "/api": {
+          target: "http://120.79.8.215:5200",
+          secure: false,
+        },
+        "/demo-react-1": "http://localhost:8081/",
+      },
+    },
+  })
+);
