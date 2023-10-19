a2d668a03f86a865b5b7c9e0f1136f5364f1c64b
niyonggui <niyg@mw-robot.com>
2023-10-19 23:37:30 +0800

    feat: 配置环境变量

--- apps/material-ui-vite-ts/vite.config.ts
+++ apps/material-ui-vite-ts/vite.config.ts
@@ -1,19 +1,42 @@
 import { getConfig } from "@packages/build/vite.config";
-import { defineConfig, mergeConfig } from "vite";
-const sharedConfig = getConfig({
-  type: "react",
-  dirname: __dirname,
-  micro: true,
-  moduleName: "system",
-});
-
+import { defineConfig, mergeConfig, loadEnv, type UserConfig } from "vite";
 // https://vitejs.dev/config/
-export default defineConfig(
-  mergeConfig(sharedConfig, {
-    server: {
-      host: true,
-      port: 8001,
-      hmr: { port: 8081 },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const sharedConfig = getConfig({
+    type: "react",
+    dirname: __dirname,
+    micro: true,
+    moduleName: "system",
+    env,
+  });
+
+  const productionConfig: UserConfig = {
+    build: {
+      target: "esnext",
+      rollupOptions: {
+        output: {
+          name: "system",
+        },
+      },
     },
-  })
-);
+  };
+  const developmentConfig: UserConfig = {
+    server: {
+      port: 8001,
+      cors: true, // 慎用
+      origin: "http://localhost:8001",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+  };
+
+  return {
+    ...mergeConfig(
+      sharedConfig,
+      mode === "production" ? productionConfig : developmentConfig
+    ),
+  };
+});
