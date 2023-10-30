f743b3dcc5033a700758c551283c4b0f6566f25a
niyonggui <niyg@mw-robot.com>
2023-10-30 10:52:56 +0800

    feat: 修改表格参数

--- apps/micro-data-management/src/pages/assignment/index.tsx
+++ apps/micro-data-management/src/pages/assignment/index.tsx
@@ -60,32 +60,124 @@ const DataTable = () => {
           overflow: "auto",
         }}
       >
-        {tableData.length && (
+        {/* {tableData.length && ( */}
+        <MuiTable
+          columns={TaskColumn}
+          data={tableData}
+          pageChange={(pages) => {
+            setPage(pages);
+            setRowData({});
+          }}
+          rowCount={rowCount}
+          // enableColumnResizing
+          defaultColumn={{
+            minSize: 100,
+            size: 100,
+            maxSize: 200,
+          }}
+          enableToolbarInternalActions={false}
+          enableColumnActions={false}
+          enableRowSelection={false}
+          muiTableBodyRowProps={({ row }) => {
+            return {
+              sx: {
+                cursor: "pointer",
+                backgroundColor:
+                  row.getValue("id") === rowData.id ? "#1e4141" : "",
+              },
+              onClick: () => {
+                setRowData(row.original);
+              },
+            };
+          }}
+          initialState={{
+            columnPinning: {
+              right: ["actions"],
+            },
+          }}
+          state={{
+            isLoading: loading,
+            showLoadingOverlay: false,
+            showProgressBars: loading,
+            pagination: { ...page },
+          }}
+          muiTablePaperProps={{
+            sx: {
+              height: "100%",
+              padding: 2,
+            },
+          }}
+          muiTableProps={{
+            sx: {},
+          }}
+          muiTableBodyProps={{
+            sx: {
+              overflow: "auto",
+            },
+          }}
+          renderTopToolbarCustomActions={() => {
+            return (
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                  p: "4px",
+                }}
+              >
+                <Button
+                  variant="outlined"
+                  size="small"
+                  color="warning"
+                  onClick={() => {
+                    setPage({
+                      pageIndex: 0,
+                      pageSize: 10,
+                    });
+                    setRowData({});
+                  }}
+                >
+                  <Refresh loading={loading}></Refresh>
+                  刷新
+                </Button>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  color="primary"
+                  onClick={() => setOpen(true)}
+                >
+                  <AddIcon />
+                  新增
+                </Button>
+              </Box>
+            );
+          }}
+        ></MuiTable>
+        {/* )} */}
+      </Grid>
+      <Grid xs={6} item container rowSpacing={2}>
+        <Grid item xs={12}>
+          {/* {tableData.length && ( */}
           <MuiTable
-            columns={TaskColumn}
-            data={tableData}
-            pageChange={(pages) => {
-              setPage(pages);
-              setRowData({});
-            }}
-            rowCount={rowCount}
+            columns={ChildTaskColumn}
+            data={rowData?.tasks || []}
             // enableColumnResizing
             defaultColumn={{
               minSize: 100,
               size: 100,
-              maxSize: 200,
+              maxSize: 300,
             }}
             enableRowSelection={false}
+            enablePagination={false}
+            enableColumnActions={false}
+            enableTopToolbar={false}
             muiTableBodyRowProps={({ row }) => {
               return {
                 sx: {
                   cursor: "pointer",
-                  backgroundColor:
-                    row.getValue("id") === rowData.id ? "#1e4141" : "",
                 },
                 onClick: () => {
-                  console.log(row.original);
-                  setRowData(row.original);
+                  setRowTask(row.original);
                 },
               };
             }}
@@ -98,7 +190,6 @@ const DataTable = () => {
               isLoading: loading,
               showLoadingOverlay: false,
               showProgressBars: loading,
-              pagination: { ...page },
             }}
             muiTablePaperProps={{
               sx: {
@@ -107,10 +198,61 @@ const DataTable = () => {
               },
             }}
             muiTableProps={{
-              sx: {},
+              sx: {
+                height: "100%",
+              },
             }}
             muiTableBodyProps={{
               sx: {
+                height: "100%",
+                overflow: "auto",
+              },
+            }}
+          ></MuiTable>
+          {/* )} */}
+          {/* 新增区域 */}
+          <AddTaskDialog open={open} onClose={() => setOpen(false)} />
+        </Grid>
+        <Grid item xs={12}>
+          {/* {tableData.length && ( */}
+          <MuiTable
+            columns={TaskPointsColumn}
+            data={actionPoints()}
+            // enableColumnResizing
+            // enableColumnResizing
+            defaultColumn={{
+              minSize: 100,
+              size: 100,
+              maxSize: 300,
+            }}
+            enableRowSelection={false}
+            enablePagination={false}
+            enableToolbarInternalActions={false}
+            enableColumnActions={false}
+            initialState={{
+              columnPinning: {
+                right: ["actions"],
+              },
+            }}
+            state={{
+              isLoading: loading,
+              showLoadingOverlay: false,
+              showProgressBars: loading,
+            }}
+            muiTablePaperProps={{
+              sx: {
+                height: "100%",
+                padding: 2,
+              },
+            }}
+            muiTableProps={{
+              sx: {
+                height: "100%",
+              },
+            }}
+            muiTableBodyProps={{
+              sx: {
+                height: "100%",
                 overflow: "auto",
               },
             }}
@@ -124,156 +266,15 @@ const DataTable = () => {
                     p: "4px",
                   }}
                 >
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    color="warning"
-                    onClick={() => {
-                      setPage({
-                        pageIndex: 0,
-                        pageSize: 10,
-                      });
-                      setRowData({});
-                    }}
-                  >
-                    <Refresh loading={loading}></Refresh>
-                    刷新
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    color="primary"
-                    onClick={() => setOpen(true)}
-                  >
+                  <Button variant="outlined" size="small" color="primary">
                     <AddIcon />
-                    新增
+                    添加任务点
                   </Button>
                 </Box>
               );
             }}
           ></MuiTable>
-        )}
-      </Grid>
-      <Grid xs={6} item container rowSpacing={2}>
-        <Grid item xs={12}>
-          {tableData.length && (
-            <MuiTable
-              columns={ChildTaskColumn}
-              data={rowData?.tasks || []}
-              // enableColumnResizing
-              defaultColumn={{
-                minSize: 100,
-                size: 100,
-                maxSize: 300,
-              }}
-              enableRowSelection={false}
-              enablePagination={false}
-              enableColumnActions={false}
-              enableTopToolbar={false}
-              muiTableBodyRowProps={({ row }) => {
-                return {
-                  sx: {
-                    cursor: "pointer",
-                  },
-                  onClick: () => {
-                    setRowTask(row.original);
-                  },
-                };
-              }}
-              initialState={{
-                columnPinning: {
-                  right: ["actions"],
-                },
-              }}
-              state={{
-                isLoading: loading,
-                showLoadingOverlay: false,
-                showProgressBars: loading,
-              }}
-              muiTablePaperProps={{
-                sx: {
-                  height: "100%",
-                  padding: 2,
-                },
-              }}
-              muiTableProps={{
-                sx: {
-                  height: "100%",
-                },
-              }}
-              muiTableBodyProps={{
-                sx: {
-                  height: "100%",
-                  overflow: "auto",
-                },
-              }}
-            ></MuiTable>
-          )}
-          {/* 新增区域 */}
-          <AddTaskDialog open={open} onClose={() => setOpen(false)} />
-        </Grid>
-        <Grid item xs={12}>
-          {tableData.length && (
-            <MuiTable
-              columns={TaskPointsColumn}
-              data={actionPoints()}
-              // enableColumnResizing
-              // enableColumnResizing
-              defaultColumn={{
-                minSize: 100,
-                size: 100,
-                maxSize: 300,
-              }}
-              enableRowSelection={false}
-              enablePagination={false}
-              enableToolbarInternalActions={false}
-              enableColumnActions={false}
-              initialState={{
-                columnPinning: {
-                  right: ["actions"],
-                },
-              }}
-              state={{
-                isLoading: loading,
-                showLoadingOverlay: false,
-                showProgressBars: loading,
-              }}
-              muiTablePaperProps={{
-                sx: {
-                  height: "100%",
-                  padding: 2,
-                },
-              }}
-              muiTableProps={{
-                sx: {
-                  height: "100%",
-                },
-              }}
-              muiTableBodyProps={{
-                sx: {
-                  height: "100%",
-                  overflow: "auto",
-                },
-              }}
-              renderTopToolbarCustomActions={() => {
-                return (
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1,
-                      p: "4px",
-                    }}
-                  >
-                    <Button variant="outlined" size="small" color="primary">
-                      <AddIcon />
-                      添加任务点
-                    </Button>
-                  </Box>
-                );
-              }}
-            ></MuiTable>
-          )}
+          {/* )} */}
         </Grid>
       </Grid>
     </Grid>
