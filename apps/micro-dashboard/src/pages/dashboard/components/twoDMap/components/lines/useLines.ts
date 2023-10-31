312b09275785c032873cb7065d7d21d01c060f74
luyao <luyao@mw-robot.com>
2023-10-31 09:43:11 +0800

    feat: 过滤重复的边

--- apps/micro-dashboard/src/pages/dashboard/components/twoDMap/components/lines/useLines.ts
+++ apps/micro-dashboard/src/pages/dashboard/components/twoDMap/components/lines/useLines.ts
@@ -3,6 +3,23 @@ import { useMemo } from 'react'
 import { useStore } from '../../store'
 import { ILineProps } from '.'
 
+const removeDuplicateLine = (edges: API.Edge[]) => {
+  const uniqueArr: API.Edge[] = []
+  const set = new Set()
+  for (let i = 0; i < edges.length; i++) {
+    const { Start, End } = edges[i]
+    const key1 = `${Start}-${End}`
+    const key2 = `${End}-${Start}`
+    if (!set.has(key1) && !set.has(key2)) {
+      set.add(key1)
+      set.add(key2)
+      uniqueArr.push(edges[i])
+    }
+  }
+
+  return uniqueArr
+}
+
 export const useLines = (edges: API.Edge[]) => {
   const { idPointMap, stageMapRatio } = useStore((state) => ({
     stageMapRatio: state.stageMapRatio,
@@ -10,7 +27,7 @@ export const useLines = (edges: API.Edge[]) => {
   }))
   const lines: ILineProps[] = useMemo(
     () =>
-      edges.map((edge) => {
+      removeDuplicateLine(edges).map((edge) => {
         const startPoint = idPointMap.get(edge.Start)
         const endPoint = idPointMap.get(edge.End)
         const line = {
