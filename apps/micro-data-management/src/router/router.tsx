f2b801cd4d99e4b06617db9a77cd553c2c7ef52f
niyonggui <niyg@mw-robot.com>
2023-11-04 17:51:56 +0800

    feat: 解决执行两次的问题

--- apps/micro-data-management/src/router/router.tsx
+++ apps/micro-data-management/src/router/router.tsx
@@ -1,81 +1,119 @@
-import { lazy } from 'react'
-import { Navigate, RouteObject } from 'react-router-dom'
+import { lazy, Suspense } from "react";
+import { Navigate, RouteObject } from "react-router-dom";
 
-const Assignment = lazy(() => import('@/pages/assignment'))
-const NotFound = lazy(() => import('@/pages/noFound'))
-const Vehicle = lazy(() => import('@/pages/vehicle'))
-const Stats = lazy(() => import('@/pages/stats'))
-const VehicleOperation = lazy(() => import('@/pages/stats/c-pages/vehicle-operation'))
-const Configuration = lazy(() => import('@/pages/configuration'))
-const Charge = lazy(() => import('@/pages/configuration/c-pages/charge'))
-const Event = lazy(() => import('@/pages/configuration/c-pages/event'))
-const Map = lazy(() => import('@/pages/configuration/c-pages/map'))
-const Point = lazy(() => import('@/pages/configuration/c-pages/point'))
-const StandByPoint = lazy(() => import('@/pages/configuration/c-pages/standByPoint'))
-const User = lazy(() => import('@/pages/configuration/c-pages/user'))
-const VehicleType = lazy(() => import('@/pages/configuration/c-pages/vehicleType'))
+const Assignment = lazy(() => import("@/pages/assignment"));
+const NotFound = lazy(() => import("@/pages/noFound"));
+const Vehicle = lazy(() => import("@/pages/vehicle"));
+const Stats = lazy(() => import("@/pages/stats"));
+const VehicleOperation = lazy(
+  () => import("@/pages/stats/c-pages/vehicle-operation")
+);
+const Configuration = lazy(() => import("@/pages/configuration"));
+const Charge = lazy(() => import("@/pages/configuration/c-pages/charge"));
+const Event = lazy(() => import("@/pages/configuration/c-pages/event"));
+const Map = lazy(() => import("@/pages/configuration/c-pages/map"));
+const Point = lazy(() => import("@/pages/configuration/c-pages/point"));
+const StandByPoint = lazy(
+  () => import("@/pages/configuration/c-pages/standByPoint")
+);
+const User = lazy(() => import("@/pages/configuration/c-pages/user"));
+const VehicleType = lazy(
+  () => import("@/pages/configuration/c-pages/vehicleType")
+);
 
 const router: RouteObject[] = [
   {
-    path: '/',
-    element: <Navigate to={'/assignment'} />
+    path: "/",
+    element: (
+      <Suspense>
+        <Navigate to={"/assignment"} />
+      </Suspense>
+    ),
   },
   {
-    path: '/assignment',
-    element: <Assignment />
+    path: "/assignment",
+    element: <Assignment />,
   },
   {
-    path: '/vehicle',
-    element: <Vehicle />
+    path: "/vehicle",
+    element: <Vehicle />,
   },
   {
-    path: '/stats',
+    path: "/stats",
     element: <Stats />,
     children: [
       {
-        path: 'vehicle-operation',
-        element: <VehicleOperation />
-      }
-    ]
+        path: "vehicle-operation",
+        element: <VehicleOperation />,
+      },
+    ],
   },
   {
-    path: '/configuration',
+    path: "/configuration",
     element: <Configuration />,
     children: [
       {
-        path: 'charge',
-        element: <Charge />
+        path: "charge",
+        element: (
+          <Suspense>
+            <Charge />
+          </Suspense>
+        ),
       },
       {
-        path: 'event',
-        element: <Event />
+        path: "event",
+        element: (
+          <Suspense>
+            <Event />
+          </Suspense>
+        ),
       },
       {
-        path: 'map',
-        element: <Map />
+        path: "map",
+        element: (
+          <Suspense>
+            <Map />
+          </Suspense>
+        ),
       },
       {
-        path: 'point',
-        element: <Point />
+        path: "point",
+        element: (
+          <Suspense>
+            <Point />
+          </Suspense>
+        ),
       },
       {
-        path: 'stand-by-point',
-        element: <StandByPoint />
+        path: "stand-by-point",
+        element: (
+          <Suspense>
+            <StandByPoint />
+          </Suspense>
+        ),
       },
       {
-        path: 'user',
-        element: <User />
+        path: "user",
+        element: (
+          <Suspense>
+            <User />
+          </Suspense>
+        ),
       },
       {
-        path: 'vehicle-type',
-        element: <VehicleType />
-      }
-    ]
+        path: "vehicle-type",
+        element: (
+          <Suspense>
+            <VehicleType />
+          </Suspense>
+        ),
+      },
+    ],
   },
   {
-    path: '*',
-    element: <NotFound />
-  }
-]
+    path: "*",
+    element: <NotFound />,
+  },
+];
 
-export default router
+export default router;
