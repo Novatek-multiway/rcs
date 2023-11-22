393920b6633e0f603686f09dafb76feb3c8ab18a
luyao <luyao@mw-robot.com>
2023-11-22 16:28:27 +0800

    build: 添加本地构建脚本

--- build-local.js
+++ build-local.js
@@ -1,17 +1,17 @@
-const { exec } = require('child_process')
-const { cp } = require('fs')
+import { cp } from 'fs'
+import { exec } from 'child_process'
 
 const apps = ['main', 'micro-dashboard', 'micro-data-management']
 
-exec('sudo pnpm build', (error) => {
-  if (error) {
-    console.error(error)
-    return
-  }
-  // 将打包的产物放到dist/${app}目录下
-  apps.forEach((app) => {
-    cp(`apps/${app}/dist`, `dist/${app}`, () => {
-      console.log(`copy ${app} success`)
-    })
-  })
+exec('pnpm build', () => {
+  apps.forEach((app) =>
+    cp(
+      `./apps/${app}/dist/`,
+      `./dist/${app}/`,
+      {
+        recursive: true
+      },
+      (err) => err
+    )
+  )
 })
