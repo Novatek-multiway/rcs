6acd6500b0e72b4b2ead4adf557a2de108192be1
luyao <luyao@mw-robot.com>
2023-12-05 09:48:08 +0800

    fix: 地图配页面修改(https://www.tapd.cn/39450798/bugtrace/bugs/view?bug_id=1139450798001001335)

--- apps/micro-data-management/src/pages/configuration/c-pages/map/index.tsx
+++ apps/micro-data-management/src/pages/configuration/c-pages/map/index.tsx
@@ -1,134 +1,108 @@
-import AddIcon from "@mui/icons-material/Add";
-import EditNoteIcon from "@mui/icons-material/EditNote";
-import { useRequest } from "ahooks";
-import {
-  ChangeActive,
-  DeleteRouteFileInfo,
-  GetChassisList,
-  GetMapOptionPageList,
-  GetRuleControlStates,
-} from "apis";
-import type { FC, ReactNode } from "react";
-import React, { memo } from "react";
-import { BaseTable, Box, Button, Switch } from "ui";
+import AddIcon from '@mui/icons-material/Add'
+import EditNoteIcon from '@mui/icons-material/EditNote'
+import { useRequest } from 'ahooks'
+import { ChangeActive, DeleteRouteFileInfo, GetChassisList, GetMapOptionPageList, GetRuleControlStates } from 'apis'
+import type { FC, ReactNode } from 'react'
+import React, { memo } from 'react'
+import { BaseTable, Box, Button, Switch } from 'ui'
 
-import DelButton from "@/component/delButton";
-import Refresh from "@/component/refreshIcon";
+import DelButton from '@/component/delButton'
+import Refresh from '@/component/refreshIcon'
 
-import AddDialog from "./components/add";
-import EditDialog from "./components/edit";
-import ImportDialog from "./components/import";
+import AddDialog from './components/add'
+import EditDialog from './components/edit'
+import ImportDialog from './components/import'
 
 interface IProps {
-  children?: ReactNode;
+  children?: ReactNode
 }
 
 const dictsTransform = (arr: [], label: string, value: string) => {
   if (!arr) {
-    return [];
+    return []
   }
   return arr.map((item) => ({
     label: String(item[label]),
-    value: item[value],
-  }));
-};
+    value: item[value]
+  }))
+}
 
 //事件配置
 const MapPage: FC<IProps> = () => {
-  const [open, setOpen] = React.useState(false);
-  const [editOpen, setEditOpen] = React.useState(false);
-  const [openImport, setOpenImport] = React.useState(false);
-  const [row, setRow] = React.useState({});
-  const {
-    data: chassisData,
-    loading,
-    run: getChass,
-  } = useRequest(() => GetMapOptionPageList({ id: 0 }));
+  const [open, setOpen] = React.useState(false)
+  const [editOpen, setEditOpen] = React.useState(false)
+  const [openImport, setOpenImport] = React.useState(false)
+  const [row, setRow] = React.useState({})
+  const { data: chassisData, loading, run: getChass } = useRequest(() => GetMapOptionPageList({ id: 0 }))
 
   const { runAsync: delFn } = useRequest(DeleteRouteFileInfo, {
-    manual: true,
-  });
-  const { data: controlStates } = useRequest(GetRuleControlStates);
+    manual: true
+  })
+  const { data: controlStates } = useRequest(GetRuleControlStates)
 
   const { runAsync: changeFn } = useRequest(ChangeActive, {
     manual: true,
     onSuccess: () => {
-      getChass();
-    },
-  });
+      getChass()
+    }
+  })
 
-  const { data: chassisList } = useRequest(GetChassisList);
+  const { data: chassisList } = useRequest(GetChassisList)
 
-  const convertChassisList = dictsTransform(chassisList?.data, "model", "id");
-  const carsList = dictsTransform(controlStates?.data, "id", "id");
+  const convertChassisList = dictsTransform(chassisList?.data, 'model', 'id')
+  const carsList = dictsTransform(controlStates?.data, 'id', 'id')
 
   const columns = [
     {
-      accessorKey: "id",
-      header: "事件ID",
+      accessorKey: 'id',
+      header: '编号'
     },
     {
-      accessorKey: "projectName",
-      header: "项目名称",
+      accessorKey: 'projectName',
+      header: '项目名称'
     },
     {
-      accessorKey: "routeName",
-      header: "地图名称",
+      accessorKey: 'routeName',
+      header: '地图名称'
     },
     {
-      accessorKey: "mapChassis",
-      header: "适用车辆",
+      accessorKey: 'mapChassis',
+      header: '适用车辆',
       Cell: ({ row }) => {
-        const { original } = row;
-        const tyles = carsList?.find(
-          (item) => item.value === original.mapChassis
-        );
-        return <>{tyles?.label || ""}</>;
-      },
+        const { original } = row
+        const tyles = carsList?.find((item) => item.value === original.mapChassis)
+        return <>{tyles?.label || 'All'}</>
+      }
     },
     {
-      accessorKey: "mapCarrier",
-      header: "适用车型",
+      accessorKey: 'mapCarrier',
+      header: '适用车型',
       Cell: ({ row }) => {
-        const { original } = row;
-        const tyles = convertChassisList?.find(
-          (item) => item.value === Number(original.mapCarrier)
-        );
-        return <>{tyles?.label || ""}</>;
-      },
+        const { original } = row
+        const tyles = convertChassisList?.find((item) => item.value === Number(original.mapCarrier))
+        return <>{tyles?.label || 'All'}</>
+      }
     },
     {
-      accessorKey: "guid",
-      header: "地图GUID",
+      accessorKey: 'guid',
+      header: '地图GUID'
     },
     {
-      accessorKey: "revision",
+      accessorKey: 'revision',
       enableColumnFilter: false,
-      header: "版本号",
+      header: '版本号'
     },
     {
-      accessorKey: "path",
-      header: "地图路径",
+      accessorKey: 'routeFile',
+      header: '路径文件名称'
     },
     {
-      accessorKey: "url",
-      header: "地图URL",
+      accessorKey: 'dwgFile',
+      header: '地图Dxf文件名'
     },
     {
-      accessorKey: "routeFile",
-      header: "路径数据文件名",
-    },
-    {
-      accessorKey: "dwgFile",
-      header: "地图Dxf文件名",
-    },
-    {
-      accessorKey: "svgFile",
-      header: "地图Svg文件名",
-    },
-    {
-      accessorKey: "isActive",
-      header: "是否激活",
+      accessorKey: 'isActive',
+      header: '是否激活',
       Cell: ({ row }) => {
         return (
           <>
@@ -137,27 +111,27 @@ const MapPage: FC<IProps> = () => {
               onChange={() =>
                 changeFn({
                   id: row.original.id,
-                  isActive: !row.original.isActive,
+                  isActive: !row.original.isActive
                 })
               }
             />
           </>
-        );
-      },
+        )
+      }
     },
     {
-      accessorKey: "actions",
-      header: "操作",
+      accessorKey: 'actions',
+      header: '操作',
       enableColumnFilter: false,
       enableSorting: false,
       Cell: ({ row }) => {
         return (
           <div
             style={{
-              display: "flex",
-              flexWrap: "nowrap",
-              gap: "0.5rem",
-              width: "100px",
+              display: 'flex',
+              flexWrap: 'nowrap',
+              gap: '0.5rem',
+              width: '100px'
             }}
           >
             <Button
@@ -165,8 +139,8 @@ const MapPage: FC<IProps> = () => {
               size="small"
               color="warning"
               onClick={() => {
-                setEditOpen(true);
-                setRow(row.original);
+                setEditOpen(true)
+                setRow(row.original)
               }}
               startIcon={<EditNoteIcon />}
             >
@@ -175,16 +149,16 @@ const MapPage: FC<IProps> = () => {
             <DelButton
               delFn={async () => {
                 await delFn({
-                  id: row.original.id,
-                });
-                getChass();
+                  id: row.original.id
+                })
+                getChass()
               }}
             />
           </div>
-        );
-      },
-    },
-  ];
+        )
+      }
+    }
+  ]
   return (
     <>
       <BaseTable
@@ -192,19 +166,19 @@ const MapPage: FC<IProps> = () => {
         data={chassisData?.data.data || []}
         muiTablePaperProps={{
           sx: {
-            height: "100%",
-            padding: 2,
-          },
+            height: '100%',
+            padding: 2
+          }
         }}
         loading={loading}
         renderTopToolbarCustomActions={() => {
           return (
             <Box
               sx={{
-                display: "flex",
-                alignItems: "center",
+                display: 'flex',
+                alignItems: 'center',
                 gap: 1,
-                p: "4px",
+                p: '4px'
               }}
             >
               <Button
@@ -212,37 +186,27 @@ const MapPage: FC<IProps> = () => {
                 size="small"
                 color="warning"
                 onClick={() => {
-                  getChass();
+                  getChass()
                 }}
               >
                 <Refresh loading={loading}></Refresh>
                 刷新
               </Button>
-              <Button
-                variant="outlined"
-                size="small"
-                color="primary"
-                onClick={() => setOpen(true)}
-              >
+              <Button variant="outlined" size="small" color="primary" onClick={() => setOpen(true)}>
                 <AddIcon />
                 新增路径文件
               </Button>
-              <Button
-                variant="outlined"
-                size="small"
-                color="success"
-                onClick={() => setOpenImport(true)}
-              >
+              <Button variant="outlined" size="small" color="success" onClick={() => setOpenImport(true)}>
                 <AddIcon />
                 上传底图文件
               </Button>
             </Box>
-          );
+          )
         }}
         initialState={{
           columnPinning: {
-            right: ["isActive", "actions"],
-          },
+            right: ['isActive', 'actions']
+          }
         }}
         enableToolbarInternalActions={true}
         enableFullScreenToggle={false}
@@ -257,7 +221,7 @@ const MapPage: FC<IProps> = () => {
         carrierData={convertChassisList}
         controlStates={carsList}
         callback={() => {
-          getChass();
+          getChass()
         }}
       />
       <EditDialog
@@ -267,12 +231,12 @@ const MapPage: FC<IProps> = () => {
         carrierData={convertChassisList}
         controlStates={carsList}
         callback={() => {
-          getChass();
+          getChass()
         }}
       />
       <ImportDialog open={openImport} onClose={() => setOpenImport(false)} />
     </>
-  );
-};
+  )
+}
 
-export default memo(MapPage);
+export default memo(MapPage)
