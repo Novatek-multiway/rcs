970257444781cf57cf469e744e1b5bef571dd52f
luyao <luyao@mw-robot.com>
2023-12-06 14:50:21 +0800

    fix: 实时刷新显示车辆任务的路径情况(https://www.tapd.cn/39450798/bugtrace/bugs/view?bug_id=1139450798001001326)

--- apps/micro-data-management/src/pages/vehicle/components/info.tsx
+++ apps/micro-data-management/src/pages/vehicle/components/info.tsx
@@ -1,136 +1,154 @@
-import { useAsyncEffect } from "ahooks";
-import { getGetCarrierTrack } from "apis";
-import * as React from "react";
-import { useDictStore } from "store";
-import {
-  BaseTable,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  TextField,
-  useTheme,
-} from "ui";
+import { getControlStateById, getGetCarrierTrack } from 'apis'
+import * as React from 'react'
+import { useDictStore } from 'store'
+import { BaseTable, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, useTheme } from 'ui'
 
 const InfoDialog: React.FC<{
-  open: boolean;
-  onClose?: () => void;
-  callback?: () => void;
-  row?: Record<string, any>;
+  open: boolean
+  onClose?: () => void
+  callback?: () => void
+  row?: Record<string, any>
 }> = ({ open, onClose = () => {}, callback, row = {} }) => {
-  const [tableData, setTableData] = React.useState([]);
-  const theme = useTheme();
-  const { dicts } = useDictStore();
+  const [tableData, setTableData] = React.useState([])
+  const theme = useTheme()
+  const { dicts } = useDictStore()
 
   const _Dict = React.useMemo(() => {
     const obj: any = {},
-      ary = ["ControlState", "GraphGenus", "RouteState"];
+      ary = ['ControlState', 'GraphGenus', 'RouteState']
     ary.forEach((key: string) => {
-      if (!dicts[key] || !dicts[key]?.length) dicts[key] = [];
+      if (!dicts[key] || !dicts[key]?.length) dicts[key] = []
       dicts[key]?.forEach((item: any) => {
-        !obj[key] && (obj[key] = {});
-        obj[key][item.value] = item.label;
-      });
-    });
-    return obj;
-  }, [dicts]);
+        !obj[key] && (obj[key] = {})
+        obj[key][item.value] = item.label
+      })
+    })
+    return obj
+  }, [dicts])
 
   React.useEffect(() => {
-    console.log("_Dict", _Dict);
-  }, [_Dict]);
+    console.log('_Dict', _Dict)
+  }, [_Dict])
 
-  useAsyncEffect(async () => {
-    if (!row?.id) return;
-    const { data } = await getGetCarrierTrack(row?.id);
-    setTableData(data || []);
-  }, [row]);
+  const [carrierControlState, setCarrierControlState] = React.useState<any>({})
+  React.useEffect(() => {
+    const timer = setInterval(async () => {
+      if (open) {
+        const carrierControlStateData = await getControlStateById(row?.id)
+        setCarrierControlState(carrierControlStateData.data || {})
+        const { data } = await getGetCarrierTrack(row?.id)
+        setTableData(data || [])
+      }
+    }, 300)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [open, row])
 
   const column = [
     {
-      accessorKey: "type",
-      id: "type",
-      header: "路径类型",
+      accessorKey: 'type',
+      id: 'type',
+      header: '路径类型',
       Cell: ({ cell }: any) => {
-        return <div>{_Dict["GraphGenus"][cell.getValue()]}</div>;
-      },
+        return <div>{_Dict['GraphGenus'][cell.getValue()]}</div>
+      }
     },
     {
-      accessorKey: "id",
-      id: "id",
-      header: "路径编号",
+      accessorKey: 'id',
+      id: 'id',
+      header: '路径编号'
     },
     {
-      accessorKey: "state1",
-      id: "state1",
-      header: "调度状态",
+      accessorKey: 'state1',
+      id: 'state1',
+      header: '调度状态',
       Cell: ({ cell }: any) => {
-        return <div>{_Dict["RouteState"][cell.getValue()]}</div>;
-      },
+        return (
+          <div style={{ color: cell.getValue() === 5 ? '#00cbca' : '#fff' }}>
+            {_Dict['RouteState'][cell.getValue()]}
+          </div>
+        )
+      }
     },
     {
-      accessorKey: "state",
-      id: "state",
-      header: "路径状态",
-    },
-  ];
+      accessorKey: 'state',
+      id: 'state',
+      header: '路径状态'
+    }
+  ]
 
   return (
     <Dialog maxWidth="md" open={open} onClose={onClose}>
       <DialogTitle>路径状态</DialogTitle>
       <DialogContent
         sx={{
-          py: `${theme.spacing(3.25)} !important`,
+          py: `${theme.spacing(3.25)} !important`
         }}
       >
-        <div style={{ display: "flex", width: "100%", gap: 20 }}>
+        <div style={{ display: 'flex', width: '100%', gap: 20 }}>
           <TextField
-            disabled
+            InputProps={{
+              readOnly: true
+            }}
             label="车辆编号"
             variant="filled"
-            defaultValue={row.id}
+            focused
+            value={carrierControlState.id}
             style={{ flex: 1 }}
           />
           <TextField
-            disabled
+            InputProps={{
+              readOnly: true
+            }}
             label="车辆位置"
             variant="filled"
-            defaultValue={row.carrierPos}
+            focused
+            value={carrierControlState.x + '-' + carrierControlState.y + '-' + carrierControlState.z}
             style={{ flex: 1 }}
           />
           <TextField
-            disabled
+            InputProps={{
+              readOnly: true
+            }}
             label="当前电量"
             variant="filled"
-            defaultValue={row.elecQuantity}
+            focused
+            value={carrierControlState.elecQuantity}
             style={{ flex: 1 }}
           />
         </div>
-        <div style={{ display: "flex", width: "100%", gap: 20, marginTop: 10 }}>
+        <div style={{ display: 'flex', width: '100%', gap: 20, marginTop: 10 }}>
           <TextField
-            disabled
+            InputProps={{
+              readOnly: true
+            }}
             label="车辆速度"
             variant="filled"
-            defaultValue={row.loadSpeed}
+            focused
+            value={carrierControlState.speed}
             style={{ flex: 1 }}
           />
           <TextField
-            disabled
+            InputProps={{
+              readOnly: true
+            }}
             label="控制状态"
             variant="filled"
+            focused
             style={{ flex: 1 }}
-            defaultValue={
-              _Dict["ControlState"]
-                ? _Dict["ControlState"][row.controlState]
-                : "-"
-            }
+            value={_Dict['ControlState'] ? _Dict['ControlState'][carrierControlState.controlState] : '-'}
           />
           <TextField
-            disabled
+            InputProps={{
+              readOnly: true
+            }}
             label="交管车辆"
             variant="filled"
+            focused
             style={{ flex: 1 }}
-            defaultValue={row.id}
+            value={carrierControlState.trafficControlCar}
           />
         </div>
         <div style={{ height: 10 }}></div>
@@ -139,16 +157,17 @@ const InfoDialog: React.FC<{
           data={tableData || []}
           enableStickyHeader={true}
           enableTopToolbar={false}
+          enablePagination={false}
           muiTablePaperProps={{
             sx: {
-              height: "100%",
-              padding: 2,
-            },
+              height: '100%',
+              padding: 2
+            }
           }}
           muiTableContainerProps={{
             sx: {
-              maxHeight: 400,
-            },
+              maxHeight: 400
+            }
           }}
         ></BaseTable>
       </DialogContent>
@@ -156,8 +175,8 @@ const InfoDialog: React.FC<{
         <Button
           color="primary"
           onClick={async () => {
-            onClose();
-            callback && callback();
+            onClose()
+            callback && callback()
           }}
         >
           保存
@@ -167,6 +186,6 @@ const InfoDialog: React.FC<{
         </Button>
       </DialogActions>
     </Dialog>
-  );
-};
-export default InfoDialog;
+  )
+}
+export default InfoDialog
--- packages/apis/modules/car/index.ts
+++ packages/apis/modules/car/index.ts
@@ -1,101 +1,99 @@
-import { request,notification } from "../../request";
-import { IResponse } from "../../type";
+import { request, notification } from '../../request'
+import { IResponse } from '../../type'
 
 /**
  * Fetches the layout from the server.
  *
  * @returns Promise of the layout object.
  */
-export {notification}
+export { notification }
 export const postGetControlStates = (data: Record<string, any>) => {
-  return request<IResponse<any>>("/Carrier/GetControlStates", {
-    method: "GET",
-    data,
-  });
-};
+  return request<IResponse<any>>('/Carrier/GetControlStates', {
+    method: 'GET',
+    data
+  })
+}
 
 export const postGetControlOptions = (data: Record<string, any>) => {
-  return request<IResponse<any>>("/Carrier/GetCarrierOptions", {
-    method: "POST",
-    data,
-  });
-};
+  return request<IResponse<any>>('/Carrier/GetCarrierOptions', {
+    method: 'POST',
+    data
+  })
+}
 
-export const getGetDictInfo = (data: string)=>{
+export const getGetDictInfo = (data: string) => {
   return request<IResponse<any>>(`/Dict/GetDictInfo?DictName=${data}`, {
-    method: "GET",
-  });
+    method: 'GET'
+  })
 }
 
 // 激活车辆的接口
-export const getSimulationCarrierLogin = (key: string)=>{
+export const getSimulationCarrierLogin = (key: string) => {
   return request<IResponse<any>>(`/Simulation/SimulationCarrierLogin?key=${key}`, {
-    method: "GET",
-  });
+    method: 'GET'
+  })
 }
 
 export const postRemoveCarrier = (data: Record<string, any>) => {
-  return request<IResponse<any>>("/Carrier/RemoveCarrier", {
-    method: "POST",
-    data,
-  });
-};
+  return request<IResponse<any>>('/Carrier/RemoveCarrier', {
+    method: 'POST',
+    data
+  })
+}
 
 // 锁车 0锁车 1解锁
 export const postUpdateCarrierState = (data: Record<string, any>) => {
-  return request<IResponse<any>>("/Carrier/UpdateCarrierState", {
-    method: "POST",
-    data,
-  });
-};
+  return request<IResponse<any>>('/Carrier/UpdateCarrierState', {
+    method: 'POST',
+    data
+  })
+}
 
 // 急停 0急停 1解除
 export const postSendRemoteStop = (data: Record<string, any>) => {
-  return request<IResponse<any>>("/Carrier/SendRemoteStop", {
-    method: "POST",
-    data,
-  });
-};
-
+  return request<IResponse<any>>('/Carrier/SendRemoteStop', {
+    method: 'POST',
+    data
+  })
+}
 
 // 创建车辆 /Carrier/CreateCarrier
 export const postCreateCarrier = (data: Record<string, any>) => {
-  return request<IResponse<any>>("/Carrier/CreateCarrier", {
-    method: "POST",
-    data,
-  });
-};
+  return request<IResponse<any>>('/Carrier/CreateCarrier', {
+    method: 'POST',
+    data
+  })
+}
 
 // 创建车辆 /Carrier/CreateCarrier
 export const postGetCarrierInfo = (id: string) => {
   return request<IResponse<any>>(`/Carrier/GetCarrierInfo?id=${id}`, {
-    method: "GET",
-  });
-};
+    method: 'GET'
+  })
+}
 
 export const postUpdateCarrier = (data: Record<string, any>) => {
-  return request<IResponse<any>>("/Carrier/UpdateCarrier", {
-    method: "POST",
-    data,
-  });
-};
-
+  return request<IResponse<any>>('/Carrier/UpdateCarrier', {
+    method: 'POST',
+    data
+  })
+}
 
 export const delCreateCarrier = (key: string) => {
   return request<IResponse<any>>(`/Carrier/DeleteCarrier?id=${key}`, {
-    method: "DELETE",
-  });
-};
+    method: 'DELETE'
+  })
+}
 
 // 获取车辆路线 GetControlState
 export const getGetCarrierTrack = (id: string) => {
   return request<IResponse<any>>(`/Carrier/GetCarrierTrack?id=${id}`, {
-    method: "GET",
-  });
-};
+    method: 'GET'
+  })
+}
 
-
-
-
-
-
+export const getControlStateById = (id: string) => {
+  return request<IResponse<any>>(`/Carrier/GetControlState?id=${id}`, {
+    method: 'GET'
+  })
+}
