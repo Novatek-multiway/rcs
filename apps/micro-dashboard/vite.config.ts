55bf239dc56a87a9068ded5f79dc2a1c41d72376
luyao <luyao@mw-robot.com>
2023-12-07 14:07:33 +0800

    fix: 修复生产环境"Failed to fetch dynamically imported module"问题

--- apps/micro-dashboard/vite.config.ts
+++ apps/micro-dashboard/vite.config.ts
@@ -43,6 +43,15 @@ export default defineConfig(({ mode }) => {
       qiankun('react18', {
         useDevMode: true
       })
-    ]
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+          entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+          assetFileNames: '[ext]/[name]-[hash].[ext]' // 资源文件像 字体，图片等
+        }
+      }
+    }
   }
 })
