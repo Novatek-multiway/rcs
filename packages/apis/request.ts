c078409f67051abd5f1c23804bee5d0f02b9b30b
luyao <luyao@mw-robot.com>
2023-12-11 16:21:23 +0800

    fix: 无感刷新token(https://www.tapd.cn/39450798/bugtrace/bugs/view?bug_id=1139450798001001381)

--- packages/apis/request.ts
+++ packages/apis/request.ts
@@ -1,17 +1,26 @@
-import { extend } from 'umi-request'
+import { RequestOptionsInit, extend } from 'umi-request'
 import { toastError } from 'utils'
 
 //全局请求参数设置
+const PREFIX = '/api'
 export const request = extend({
   timeout: 60000,
   // 记得区分开发环境与生产环境
-  prefix: '/api'
+  prefix: PREFIX
 })
 
+let isRefreshingToken = false // 是否正在刷新token
+const requestQueue: {
+  url: string
+  options: RequestOptionsInit
+  resolve: (value: Response | PromiseLike<Response>) => any
+  reject: (reason?: any) => void
+}[] = [] // 请求队列
 request.interceptors.request.use((url, options) => {
-  const userInfo = localStorage.getItem('userInfo')
+  const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}')
   const headers: any = options.headers
-  headers['Authorization'] = JSON.parse(userInfo || '{}').jwtToken
+
+  headers['Authorization'] = userInfo.jwtToken
   return {
     url,
     options: {
@@ -21,7 +30,7 @@ request.interceptors.request.use((url, options) => {
   }
 })
 
-request.interceptors.response.use(async (response) => {
+request.interceptors.response.use(async (response, options) => {
   const { status } = response
   if (status === 200) {
     const data = await response.clone().json()
@@ -31,6 +40,31 @@ request.interceptors.response.use(async (response) => {
     }
     return data
   } else {
+    if (status === 401) {
+      if (!isRefreshingToken) {
+        // 重新登录
+        const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}')
+        isRefreshingToken = true
+        //刷新token
+        const res = await request('/User/ReLogin', {
+          method: 'POST',
+          data: userInfo
+        })
+        localStorage.setItem('userInfo', JSON.stringify(res.data))
+        isRefreshingToken = false
+        const response = request(options.url, options)
+
+        // 重新登录后，将队列中的请求重新发出
+        requestQueue.forEach((cb) => cb.resolve(request(cb.url, cb.options)))
+
+        return response
+      } else {
+        // 正在刷新token，将返回一个未执行resolve的promise
+        return new Promise((resolve, reject) => {
+          requestQueue.push({ url: options.url, options, resolve, reject })
+        })
+      }
+    }
     toastError(response.statusText)
     return Promise.reject(response.statusText)
   }
