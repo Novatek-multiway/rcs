b00b9b1bab5f1e94cb5a362b2f769dc59e7f893a
luyao <luyao@mw-robot.com>
2023-12-22 15:45:42 +0800

    fix: 修复语言更新字典没有重新获取问题

--- apps/micro-data-management/src/global.tsx
+++ apps/micro-data-management/src/global.tsx
@@ -1,29 +1,41 @@
-import { useRequest } from "ahooks";
-import { getDicts } from "apis";
-import { useDictStore } from "store";
+import { useVoerkaI18n } from '@voerkai18n/react'
+import { useRequest, useUpdateEffect } from 'ahooks'
+import { getDicts } from 'apis'
+import { useState } from 'react'
+import { useDictStore } from 'store'
 
 // Converts an object with arrays of dictValueI and dictLabel to an object with arrays of value and label
 export const dictsTransform = (obj: Record<string, any[]>) => {
-  const keys = Object.keys(obj);
-  const newDicts: Record<string, { value: number; label: string }[]> = {};
+  const keys = Object.keys(obj)
+  const newDicts: Record<string, { value: number; label: string }[]> = {}
   keys.forEach((key) => {
     newDicts[key] = obj[key].map((item) => ({
       value: item.dictValueI,
-      label: item.dictLabel,
-    }));
-  });
-  return newDicts;
-};
+      label: item.dictLabel
+    }))
+  })
+  return newDicts
+}
 const GlobalContext = () => {
-  const { setDicts } = useDictStore();
+  const { setDicts } = useDictStore()
+  const { activeLanguage } = useVoerkaI18n()
+  console.log('re-render')
+  const [ready, setReady] = useState(true)
   useRequest(() => getDicts({}), {
     onSuccess: (res) => {
       if (res.data) {
-        setDicts(dictsTransform(res.data));
+        setDicts(dictsTransform(res.data))
+        setReady(false)
       }
     },
-  });
-  return null;
-};
+    ready,
+    cacheKey: activeLanguage
+  })
 
-export default GlobalContext;
+  useUpdateEffect(() => {
+    setReady(true)
+  }, [activeLanguage])
+  return null
+}
+
+export default GlobalContext
