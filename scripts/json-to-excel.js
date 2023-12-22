384164ecd91d5994834fdb514f896c59c358f740
luyao <luyao@mw-robot.com>
2023-12-22 17:54:23 +0800

    build: 添加json-to-excel脚本

---
+++ scripts/json-to-excel.js
@@ -0,0 +1,46 @@
+import fs from 'fs'
+import path from 'path'
+import json2xls from 'json2xls'
+
+const jsonPaths = [
+  '/apps/main/src/languages/translates/default.json',
+  '/apps/micro-dashboard/src/languages/translates/default.json',
+  '/apps/micro-data-management/src/languages/translates/default.json'
+]
+
+// Process each JSON file
+jsonPaths.forEach((jsonPath) => {
+  const jsonData = []
+
+  // Add first row with jsonPath
+  jsonData.push([jsonPath, '', '', '', ''])
+  jsonData.push(['zh', 'en', 'jp', 'kor', '$files'])
+
+  // Read JSON data from file
+  const data = fs.readFileSync(path.join(process.cwd(), jsonPath), 'utf-8')
+
+  try {
+    // Parse JSON data
+    const json = JSON.parse(data)
+
+    // Add JSON data to the array
+    Object.entries(json).forEach(([chinesePhrase, translations]) => {
+      const row = [chinesePhrase, translations.en, translations.kor, translations.jp, translations.$files.join(',')]
+      jsonData.push(row)
+    })
+
+    // Convert JSON data to Excel format
+    const xls = json2xls(jsonData)
+
+    // Extract the desired portion of the jsonPath for the filename
+    const filename = jsonPath.match(/\/apps\/(.*)\/src/)[1] + '.xlsx'
+
+    // Save Excel file with the extracted filename
+    const excelPath = path.join(process.cwd(), filename)
+    fs.writeFileSync(excelPath, xls, 'binary')
+
+    console.log('JSON file converted to Excel:', excelPath)
+  } catch (error) {
+    console.error('Error parsing JSON file:', jsonPath, error)
+  }
+})
